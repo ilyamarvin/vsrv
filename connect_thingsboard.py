8cfdce894d23f98f3cda27c7e032d800465eda46
IlyaMarvin <ilya.sereda2002@mail.ru>
2021-11-30 21:34:04 +0300

    Trying to simulate sensor data using MQTT and ThingsBoard

---
+++ connect_thingsboard.py
@@ -0,0 +1,40 @@
+import random
+from paho.mqtt import client as mqtt_client
+import os
+import json
+import time
+
+THINGSBOARD_HOST = 'demo.thingsboard.io'
+ACCESS_TOKEN = 'Ds5EWYtKEvV3YqWkQdWd'
+port = 1883
+
+from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
+
+
+telemetry = {"temperature": 41.7, "enabled": False, "currentFirmwareVersion": "v1.2.2"}
+
+#
+# # Connect to ThingsBoard
+# client.connect()
+# # Sending telemetry without checking the delivery status
+# client.send_telemetry(telemetry)
+# # Sending telemetry and checking the delivery status (QoS = 1 by default)
+# result = client.send_telemetry(telemetry)
+# # get is a blocking call that awaits delivery status
+# success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
+# # Disconnect from ThingsBoard
+# client.disconnect()
+
+
+from time import sleep
+from tb_device_mqtt import TBDeviceMqttClient
+
+
+def callback(result):
+    print(result)
+
+client = TBDeviceMqttClient(THINGSBOARD_HOST, ACCESS_TOKEN)
+client.connect()
+client.subscribe_to_attribute("parameter", callback)
+while True:
+    sleep(1)
--- sensor_simulation.py
+++ sensor_simulation.py
@@ -3,6 +3,7 @@ from paho.mqtt import client as mqtt_client
 import os
 import json
 import time
+from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
 
 # Работа датчиков
 smart_sensors = True
@@ -14,43 +15,54 @@ ACCESS_TOKEN = 'Ds5EWYtKEvV3YqWkQdWd'
 port = 1883
 
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to MQTT Broker!")
-    else:
-        print("Failed to connect, return code %d\n", rc)
+class Sensor:
+    def __init__(self, id_sensor, params):
+        self.id_sensor = id_sensor
+        self.params = params
 
 
-def on_publish(client, userdata, result):
-    print("Data from sensor published to ThingsBoard\n")
-    pass
+sensors = {
+    'sensor_1': Sensor(1, start_value),
+    'sensor_2': Sensor(2, start_value),
+    'sensor_3': Sensor(3, start_value),
+    'sensor_4': Sensor(4, start_value),
+}
 
-client = mqtt_client.Client("Parking_sensor_1")
-client.on_connect = on_connect
-client.on_publish = on_publish
-client.username_pw_set(ACCESS_TOKEN)
-client.connect(THINGSBOARD_HOST, port, keepalive=60)
 
-client.loop_start()
-while smart_sensors:
-    params = random.randint(start_value - 100, start_value + 100)
-    data = "{\"parameter\":" + str(params) + "}"
-    client.publish("v1/devices/me/telemetry", data)
-    time.sleep(5)
-client.loop_stop()
-client.disconnect()
+def simulation(x):
+    if x == 1:
+        while smart_sensors:
+            params = random.randint(start_value - 25, start_value + 25)
+            data = "{\"parameter\":" + str(params) + "}"
+            client.publish("v1/devices/me/telemetry", data)
+            time.sleep(5)
+    elif x == 0:
+        while smart_sensors:
+            params = random.randint(start_value*3-25, start_value*3+25)
+            data = "{\"parameter\":" + str(params) + "}"
+            client.publish("v1/devices/me/telemetry", data)
+            time.sleep(5)
 
-# def simulation(x):
-#     if x == 1:
-#         while smart_sensors:
-#             params = random.randint(start_value - 100, start_value + 100)
-#             data = "{\"parameter\":", params, "}"
-#             client.publish("v1/devices/me/telemetry", data)
-#             time.sleep(5)
-#     elif x == 0:
-#         while smart_sensors:
-#             params = random.randint(start_value * 3, start_value * 4)
-#             data = "{\"parameter\":", params, "}"
-#             client.publish("v1/devices/me/telemetry", data)
-#             time.sleep(5)
 
+def empty_parking_slots():
+    while smart_sensors:
+        params1 = random.randint(start_value - 25, start_value + 25)
+        params2 = random.randint(start_value - 25, start_value + 25)
+        params3 = random.randint(start_value - 25, start_value + 25)
+        params4 = random.randint(start_value - 25, start_value + 25)
+        telemetry = {list(sensors.keys())[0]: params1, list(sensors.keys())[1]: params2,
+                     list(sensors.keys())[2]: params3, list(sensors.keys())[3]: params4}
+        client = TBDeviceMqttClient(THINGSBOARD_HOST, ACCESS_TOKEN)
+        # Connect to ThingsBoard
+        client.connect()
+        # Sending telemetry without checking the delivery status
+        client.send_telemetry(telemetry)
+        # Sending telemetry and checking the delivery status (QoS = 1 by default)
+        result = client.send_telemetry(telemetry)
+        # get is a blocking call that awaits delivery status
+        success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
+        # Disconnect from ThingsBoard
+        client.disconnect()
+
+
+empty_parking_slots()
